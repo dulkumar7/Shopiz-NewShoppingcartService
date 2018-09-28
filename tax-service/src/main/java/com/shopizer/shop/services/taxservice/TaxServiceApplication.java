d096dba1f9cedd835f5813c4208056b6f44dc9c6
hamidouNTTDATA <hamidou.barry@nttdata.com>
2018-09-27 22:14:03 -0500

    Integrate code with Jenkins and PCF --testing Phase3

--- tax-service/src/main/java/com/shopizer/shop/services/taxservice/TaxServiceApplication.java
+++ tax-service/src/main/java/com/shopizer/shop/services/taxservice/TaxServiceApplication.java
@@ -2,12 +2,12 @@ package com.shopizer.shop.services.taxservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableEurekaClient
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories( basePackages = "com.shopizer.shop.services.taxservice.repository")
 public class TaxServiceApplication {
